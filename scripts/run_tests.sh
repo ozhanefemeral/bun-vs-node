35f58e1d634ab69559c471e4447698045cba68ed
Ozhan Efe Meral <ozhanefemeral@gmail.com>
2024-08-22 12:33:14 +0200

    use local test over docker

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -22,6 +22,8 @@ echo "Running HTTP tests..."
 $BASE_DIR/run_http_tests.sh
 
 echo "Running package manager tests..."
-$BASE_DIR/run_package_manager_tests.sh
+$BASE_DIR/local/run_package_manager_tests.sh
+# You can run the following command to run package manager tests on Docker. Make sure you uncomment the line below and comment the line above.
+# $BASE_DIR/run_package_manager_tests.sh
 
 echo "All tests completed. Results are saved in their respective directories under /app/results."
