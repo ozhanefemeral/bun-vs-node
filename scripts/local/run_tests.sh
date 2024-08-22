c9bd648b2d4717e50e529e47d6e8498d9a92035e
Ozhan Efe Meral <ozhanefemeral@gmail.com>
2024-08-22 19:27:45 +0200

    chore: Update run_tests.sh to use relative paths

--- scripts/local/run_tests.sh
+++ scripts/local/run_tests.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-# Set the base directory
-BASE_DIR="/scripts/local"
+BASE_DIR="$(dirname "$0")"
 
-chmod +x $BASE_DIR/*.sh
+chmod +x "$BASE_DIR"/*.sh
 
 if ! command -v bun &> /dev/null || ! command -v node &> /dev/null; then
     echo "Both Bun and Node.js are required to run these tests. Exiting."
@@ -13,17 +12,15 @@ fi
 echo "Starting all benchmarks..."
 
 echo "Running basic tests..."
-$BASE_DIR/run_basic_tests.sh
+"$BASE_DIR"/run_basic_tests.sh
 
 echo "Running file tests..."
-$BASE_DIR/run_file_tests.sh
+"$BASE_DIR"/run_file_tests.sh
 
 echo "Running HTTP tests..."
-$BASE_DIR/run_http_tests.sh
+"$BASE_DIR"/run_http_tests.sh
 
 echo "Running package manager tests..."
-$BASE_DIR/local/run_package_manager_tests.sh
-# You can run the following command to run package manager tests on Docker. Make sure you uncomment the line below and comment the line above.
-# $BASE_DIR/run_package_manager_tests.sh
+"$BASE_DIR"/run_package_manager_tests.sh
 
 echo "All tests completed. Results are saved in their respective directories under /app/results."
